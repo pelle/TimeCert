fa054ae0e7170087bab3c0a8754f9fcd51889eaf
Pelle Braendgaard <pelleb@gmail.com>
2009-05-28 14:26:59 -0400

    added contenture and google ads tracking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def source_code_select(content,options={})
-    "<textarea #{ {:cols=>70,:rows=>2,:onclick=>'this.select()'}.merge(options).collect{|k,v| "#{k}=\"#{v}\""}.join(' ') }>#{content}</textarea>"
+    "<textarea #{ {:cols=>100,:rows=>3,:onclick=>'this.select()'}.merge(options).collect{|k,v| "#{k}=\"#{v}\""}.join(' ') }>#{content}</textarea>"
   end
 end
