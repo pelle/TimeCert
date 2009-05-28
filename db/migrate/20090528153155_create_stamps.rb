f66190c0e6f7f38fa5f59e316c6d841da2d001f2
Pelle Braendgaard <pelleb@gmail.com>
2009-05-28 15:40:18 -0400

    Removed force from table creation

--- db/migrate/20090528153155_create_stamps.rb
+++ db/migrate/20090528153155_create_stamps.rb
@@ -1,6 +1,6 @@
 class CreateStamps < ActiveRecord::Migration
   def self.up
-    create_table :stamps, :id => false, :force => true do |t|
+    create_table :stamps, :id => false, :force => false do |t|
       t.column "digest","char(40) PRIMARY KEY"
       t.timestamp "created_at", :null => false
     end
