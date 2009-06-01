6b9e0e5b44d113e0d2ecfa469c7f795a2c97ebb6
Pelle Braendgaard <pelleb@gmail.com>
2009-05-31 22:17:02 -0400

    Added som developer documentation and cleaned up a bit.

---
+++ app/controllers/developers_controller.rb
@@ -0,0 +1,10 @@
+class DevelopersController < ApplicationController
+  caches_page :index,:ruby,:python
+  
+  def index
+  end
+  
+  def ruby
+  end
+
+end
--- app/helpers/digests_helper.rb
+++
@@ -1,2 +0,0 @@
-module DigestsHelper
-end
