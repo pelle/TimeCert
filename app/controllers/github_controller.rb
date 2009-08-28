14080c4e4ced4e77df9006e743350dd9043b60c0
Pelle Braendgaard <pelleb@gmail.com>
2009-08-28 12:49:16 -0400

    Added github post commit hook. This is just a quick test

---
+++ app/controllers/github_controller.rb
@@ -0,0 +1,19 @@
+class GithubController < ApplicationController
+  caches_page :show
+  
+  # extracts all digest from payload and timestamps them
+  def create
+    head :ok # Return straight away
+    begin
+      payload=JSON.parse(params[:payload])
+      Stams.by_digest payload["before"]
+      Stams.by_digest payload["after"]
+      payload["commits"].each do |c| 
+        Stams.by_digest( c["id"])
+      end
+    rescue
+      logger.error "Error parsing payload from github"
+    end
+  end
+
+end
