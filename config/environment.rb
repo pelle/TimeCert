9ffdf51c9ef601c649887c16e7868e70acf2cdfd
Pelle Braendgaard <pelleb@gmail.com>
2010-11-21 15:42:30 -0500

    bumped to rails 2.3.10 temporarily and a few other changes

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -17,9 +17,9 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem 'haml', :lib => 'haml'
+  config.gem 'haml'
   config.gem 'json'
-  config.gem 'chriseppstein-compass', :version => '>= 0.8.12', :lib => 'compass', :source => "http://gems.github.com"
+  config.gem 'compass'
 #  config.gem 'git'
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
