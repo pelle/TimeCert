1a6bcb646fe624d36de7db139129aa4f2d864590
Pelle Braendgaard <pelle@stakeventures.com>
2010-12-08 12:10:22 -0500

    upgraded to use 1.9.2

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@
 # This preamble is the current preamble for Rails 3 apps; edit as needed.
 source 'http://rubygems.org'
 
-gem 'mysql2'
 gem 'rails', '3.0.3'
 
 gem 'haml'
@@ -13,7 +12,6 @@ gem 'exceptional'
 gem 'configuration', "1.1.0"
 gem 'RedCloth'
 gem 'redis'
-gem 'system_timer'
 
 group :test do
   gem "rspec-rails"
--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,10 @@ module Timecert
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.middleware.use Rack::Deflater
+    
+    [ Rack::Lock, ActionDispatch::Cookies, ActionDispatch::Session::CookieStore, ActionDispatch::Flash].each do |m|
+#      config.middleware.delete(m)
+    end
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
